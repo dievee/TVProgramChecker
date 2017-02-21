7080e89c92fe5b362f2d85076c73fdf32886d351
dievee <nex7440@gmail.com>
2017-02-21 23:33:11 +0200

    added reduce image resolution to picture manager

--- TVProgramChecker/Managers/PictureManager.cs
+++ TVProgramChecker/Managers/PictureManager.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
@@ -9,24 +11,35 @@ namespace TVProgramChecker.Managers
 {
     class PictureManager
     {
-        public string ReduceImageResolution(ShowData sd)
+        public void ReduceImageResolution(ShowData sd)
         {
             int i = 0;
             foreach( Show s in sd.Shows)
             {
-                DownloadImage( s.image.Preview, i.ToString() );
+               string path = DownloadImage( s.image.Preview, i.ToString() );
 
+                using (Bitmap bitmap = new Bitmap(path))
+                {
+                    Size size = new Size(bitmap.Width / 6, bitmap.Height / 6);
+                    using (Bitmap newBitmap = new Bitmap(bitmap, size))
+                    {
+                        newBitmap.Save("n" + path);
+                    }
+                }
+
+                File.Delete(path);
                 i++;
             }
 
-            return null;
         }
 
-        public void DownloadImage(string url, string fileName)
+        public string DownloadImage(string url, string fileName)
         {
             using (WebClient client = new WebClient())
             {
-                client.DownloadFile(url,  fileName + ".jpg");  
+                client.DownloadFile(url,  fileName + ".jpg");
+
+                return fileName + ".jpg";
             }
         }
     }
